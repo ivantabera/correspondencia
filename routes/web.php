32fbdd992be012de92d6cdcf8d64cc50bfb47e88
Ivan tabera <itabera@doopla.mx>
2020-01-10 15:03:42 -0600

    cambios a correspondencia

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Correspondencia', function () {  //ruta
+    return view('correspondencia'); //nombre de la carpeta
+});
