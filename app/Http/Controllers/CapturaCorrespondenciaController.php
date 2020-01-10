64bca2bde5ab2b190536500a8b8c2c04d8f43b52
Ivan tabera <itabera@doopla.mx>
2020-01-10 14:52:53 -0600

    un modelo nuevo con controlador y recursos y se migro por medio de comandos de laravel

---
+++ app/Http/Controllers/CapturaCorrespondenciaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\capturaCorrespondencia;
+use Illuminate\Http\Request;
+
+class CapturaCorrespondenciaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\capturaCorrespondencia  $capturaCorrespondencia
+     * @return \Illuminate\Http\Response
+     */
+    public function show(capturaCorrespondencia $capturaCorrespondencia)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\capturaCorrespondencia  $capturaCorrespondencia
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(capturaCorrespondencia $capturaCorrespondencia)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\capturaCorrespondencia  $capturaCorrespondencia
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, capturaCorrespondencia $capturaCorrespondencia)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\capturaCorrespondencia  $capturaCorrespondencia
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(capturaCorrespondencia $capturaCorrespondencia)
+    {
+        //
+    }
+}
