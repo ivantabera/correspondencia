ae3705640567cad74d129bf87e034ee88ea5be6f
Ivan tabera <ivantabera19@gmail.com>
2020-02-13 10:27:31 -0600

    Seeder correcto

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Caffeinated\Shinobi\Contracts\Permission;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -12,60 +13,66 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
+
         //Usuarios
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Navegar correspondencia',
             'slug'          => 'correspondencia.index',
             'description'   => 'Lista toda la correspondencia del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Ver pdf de correspondencia',
             'slug'          => 'correspondencia.pdf',
             'description'   => 'PDF de toda la correspondencia del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Editar correspondencia',
             'slug'          => 'correspondencia.edit',
             'description'   => 'Editar toda la correspondencia del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Eliminar roles',
             'slug'          => 'correspondencia.destroy',
             'description'   => 'Elimina cualquier roles del sistema',
         ]);
 
         //Roles
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Navegar roles',
             'slug'          => 'roles.index',
             'description'   => 'Lista todos los roles del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Ver pdf de roles',
             'slug'          => 'roles.show',
             'description'   => 'ver de todos los roles del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Crear roles',
             'slug'          => 'roles.create',
             'description'   => 'Crea todos los roles del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Editar roles',
             'slug'          => 'roles.edit',
             'description'   => 'Editar todos los roles del sistema',
         ]);
 
-        Permission::create([
+        DB::table('permissions')->insert([
             'name'          => 'Eliminar roles',
             'slug'          => 'roles.destroy',
             'description'   => 'Elimina cualquier rol del sistema',
+        ]);
+
+
+        $this->call([
+            Permission::class,
         ]);
     }
 }
